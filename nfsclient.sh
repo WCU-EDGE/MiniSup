4ccac7a772185ef25585414e23688b6ae079b760
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2018-10-05 05:01:33 -0400

    Sleep in nfsclient

--- nfsclient.sh
+++ nfsclient.sh
@@ -2,8 +2,11 @@
 
 yum install -y nfs-utils nfs-utils-lib
 chkconfig nfs on
+sleep 30
 service rpcbind start
+sleep 30
 service nfs start
+sleep 30
 mkdir /software
 mkdir /scratch
 mount -t nfs 192.168.1.1:/users/jk880380/software /software
