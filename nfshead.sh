77cb10f4f02a1c26d8948454fad77bb3560297eb
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2018-10-05 16:37:54 -0400

    Head; Use loop

--- nfshead.sh
+++ nfshead.sh
@@ -5,51 +5,44 @@ chkconfig nfs on
 service rpcbind start
 service nfs start
 mkdir /software
-echo "/software 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-echo "/software 192.168.1.3(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-echo "/software 192.168.1.4(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-echo "/software 192.168.1.5(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-echo "/software 192.168.1.6(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-echo "/software 192.168.1.7(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-echo "/software 192.168.1.8(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-echo "/software 192.168.1.9(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-echo "/software 192.168.1.10(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-echo "/software 192.168.1.11(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-echo "/software 192.168.1.12(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-echo "/software 192.168.1.13(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-echo "/software 192.168.1.14(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-#mkdir /users/jk880380/software
-#echo "/users/jk880380/software 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-#echo "/users/jk880380/software 192.168.1.3(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-#echo "/users/jk880380/software 192.168.1.4(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-#echo "/users/jk880380/software 192.168.1.5(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-#echo "/users/jk880380/software 192.168.1.6(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-#echo "/users/jk880380/software 192.168.1.7(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-#echo "/users/jk880380/software 192.168.1.8(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-#echo "/users/jk880380/software 192.168.1.9(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-#echo "/users/jk880380/software 192.168.1.10(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-#echo "/users/jk880380/software 192.168.1.11(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-#echo "/users/jk880380/software 192.168.1.12(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-#echo "/users/jk880380/software 192.168.1.13(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
-#echo "/users/jk880380/software 192.168.1.14(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+
+# Create the permissions file for the NFS directory.
+computes=$(($1 + 1))
+for i in $(seq $computes)
+do
+  st='/software 192.168.1.'
+  st+=$(($i))
+  st+='(rw,sync,no_root_squash,no_subtree_check)'
+  echo $st >> /etc/exports
+done
+
+#echo "/software 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.3(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.4(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.5(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.6(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.7(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.8(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.9(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.10(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.11(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.12(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.13(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.14(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+
 exportfs -a
 echo "Done" >> /users/jk880380/headdoneNFS.txt
+
 set -x
 sudo yum -y group install "Development Tools"
-#cd /users/jk880380/software
 cd /software
 sudo wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.2.tar.gz
 sudo tar xzf openmpi-3.1.2.tar.gz
 cd openmpi-3.1.2
-#sudo ./configure --prefix=/opt/openmpi/3.1.2
-#sudo ./configure --prefix=/users/jk880380/software
 sudo ./configure --prefix=/software
 sudo make
 sudo make all install
-#echo "export PATH='$PATH:/users/jk880380/software/bin'" >> /users/jk880380/.bashrc
 echo "export PATH='$PATH:/software/bin'" >> /users/jk880380/.bashrc
-#echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/opt/openmpi/3.1.2/lib/'" >> /users/jk880380/.bashrc
-#echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/users/jk880380/software/lib/'" >> /users/jk880380/.bashrc
 echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/lib/'" >> /users/jk880380/.bashrc
 cd ..
 sudo rm -Rf openmpi-3.1.2
