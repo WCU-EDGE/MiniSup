834395ff0c9e848f85f64881575fd4c36baad382
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2018-10-05 07:43:58 -0400

    Client: Add mpi to path

--- nfsclient.sh
+++ nfsclient.sh
@@ -12,4 +12,14 @@ mkdir /scratch
 sleep 600
 mount -t nfs 192.168.1.1:/users/jk880380/software /software
 mount -t nfs 192.168.1.2:/users/jk880380/scratch /scratch
-echo "Done" >> /users/jk880380/clientdone.txt
+
+while [ ! -d /software/flagdir ]
+do
+  sleep 30
+done
+
+echo "export PATH='$PATH:/software/bin'" >> /users/jk880380/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/lib/'" >> /users/jk880380/.bashrc
+source /users/jk880380/.bashrc
+
+#echo "Done" >> /users/jk880380/clientdone.txt
