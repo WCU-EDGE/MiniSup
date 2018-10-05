7effca510330d6c9b82379924485f5ee2e6affcc
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2018-10-05 07:41:34 -0400

    Storage: add MPI to path

--- nfsstorage.sh
+++ nfsstorage.sh
@@ -17,6 +17,8 @@ echo "/users/jk880380/scratch 192.168.1.9(rw,sync,no_root_squash,no_subtree_chec
 echo "/users/jk880380/scratch 192.168.1.10(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 echo "/users/jk880380/scratch 192.168.1.11(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 echo "/users/jk880380/scratch 192.168.1.12(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+echo "/users/jk880380/scratch 192.168.1.13(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+echo "/users/jk880380/scratch 192.168.1.14(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 exportfs -a
 sleep 600
 mkdir /software
@@ -27,5 +29,9 @@ while [ ! -d /software/flagdir ]
 do
   sleep 30
 done
+
+echo "export PATH='$PATH:/software/bin'" >> /users/jk880380/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/lib/'" >> /users/jk880380/.bashrc
+source /users/jk880380/.bashrc
 
 echo "Done" >> /users/jk880380/storagedoneMPI.txt
