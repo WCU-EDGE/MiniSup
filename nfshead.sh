e75dc4ea7cc22e715e0110de62cc75bb0dc67ada
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2018-10-05 07:04:11 -0400

    Move MPI install into head's main sh

--- nfshead.sh
+++ nfshead.sh
@@ -18,3 +18,21 @@ echo "/users/jk880380/software 192.168.1.11(rw,sync,no_root_squash,no_subtree_ch
 echo "/users/jk880380/software 192.168.1.12(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 exportfs -a
 echo "Done" >> /users/jk880380/headdoneNFS.txt
+set -x
+sudo yum -y group install "Development Tools"
+cd /users/jk880380/software
+sudo wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.2.tar.gz
+sudo tar xzf openmpi-3.1.2.tar.gz
+cd openmpi-3.1.2
+#sudo ./configure --prefix=/opt/openmpi/3.1.2
+sudo ./configure --prefix=/users/jk880380/software
+sudo make
+sudo make all install
+echo "export PATH='$PATH:/opt/openmpi/3.1.2/bin'" >> /users/jk880380/.bashrc
+#echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/opt/openmpi/3.1.2/lib/'" >> /users/jk880380/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/users/jk880380/software/lib/'" >> /users/jk880380/.bashrc
+cd ..
+sudo rm -Rf openmpi-3.1.2
+sudo rm -Rf openmpi-3.1.2.tar.gz
+mkdir /users/jk880380/software/flagdir
+echo "Done" >> /users/jk880380/headdoneMPI.txt
