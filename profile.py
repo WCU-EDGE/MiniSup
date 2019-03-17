b6a9398ba6f19b466f4b82ecf412c14fee0f85b1
Linh B Ngo <linh.b.ngo@gmail.com>
2019-03-17 11:00:14 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -13,16 +13,7 @@ request = pc.makeRequestRSpec()
 
 tourDescription = \
 """
-This profile provides the template for a full research cluster with head node, scheduler, compute nodes, and shared file systems.
-First node (head) should contain: 
-- Shared home directory using Networked File System
-- Management server for SLURM
-Second node (metadata) should contain:
-- Metadata server for SLURM
-Third node (storage):
-- Shared software directory (/software) using Networked File System
-Remaining three nodes (computing):
-- Compute nodes  
+This profile provides the template for a compute node with Docker installed on Ubuntu 18.04
 """
 
 #
@@ -32,40 +23,12 @@ tour = IG.Tour()
 tour.Description(IG.Tour.TEXT,tourDescription)
 request.addTour(tour)
 
-prefixForIP = "192.168.1."
-
-link = request.LAN("lan")
-
-for i in range(6):
-  if i == 0:
-    node = request.XenVM("head")
-    node.routable_control_ip = "true"
-  elif i == 1:
-    node = request.XenVM("metadata")
-  elif i == 2:
-    node = request.XenVM("storage")
-  else:
-    node = request.XenVM("compute-" + str(i-2))
-    node.cores = 4
-    node.ram = 4096
-    
-  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
+node = request.RawPC("node")
+node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+node.routable_control_ip = "true"
   
-  iface = node.addInterface("if" + str(i))
-  iface.component_id = "eth1"
-  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-  link.addInterface(iface)
-  
-  node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/passwordless.sh"))
-  node.addService(pg.Execute(shell="sh", command="sudo /local/repository/passwordless.sh"))
-  node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_mpi.sh"))
-  node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_mpi.sh"))
-  
-  # This code segment is added per Benjamin Walker's solution to address the StrictHostKeyCheck issue of ssh
-  # node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/ssh_setup.sh"))
-  # node.addService(pg.Execute(shell="sh", command="sudo -H -u lngo bash -c '/local/repository/ssh_setup.sh'"))
- 
-  node.addService(pg.Execute(shell="sh", command="sudo su lngo -c 'cp /local/repository/source/* /users/lngo'"))
+node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/passwordless.sh"))
+node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_mpi.sh"))
   
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
