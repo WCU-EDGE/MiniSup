97d149482d0f06ee8b447c1554aaf909e9f0ef94
Linh B Ngo <linh.b.ngo@gmail.com>
2019-03-17 11:02:50 -0400

    Update and rename install_mpi.sh to install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/ linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# the username needs to be changed
+sudo usermod -aG docker lngo
