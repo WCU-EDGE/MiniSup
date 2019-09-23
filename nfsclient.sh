e7ac119ed13d5b80cabe9ee0e8e4a1f89de607fd
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-09-23 06:05:59 -0400

    Create nfsclient.sh

---
+++ nfsclient.sh
@@ -0,0 +1,29 @@
+
+#!/bin/sh
+
+apt-get install -y nfs-utils nfs-utils-lib
+#yum install -y nfs-utils nfs-utils-lib
+chkconfig nfs on
+service rpcbind start
+service nfs start
+mkdir /software
+mkdir /scratch
+sleep 600
+mount -t nfs 192.168.1.1:/software /software
+mount -t nfs 192.168.1.2:/scratch /scratch
+
+while [ ! -d /software/flagdir ]
+do
+  sleep 30
+done
+
+echo "export PATH='$PATH:/software/bin'" >> /users/jk880380/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/lib/'" >> /users/jk880380/.bashrc
+source /users/jk880380/.bashrc
+
+#while [ ! -d /scratch/flagdir ]
+#do
+#  sleep 30
+#done
+
+#echo "Done" >> /users/jk880380/clientdone.txt
