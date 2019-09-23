29c04cab389f22ee0adbda95adfba0a9bfa0dfd7
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-09-23 05:43:37 -0400

    remove man-db

--- install_docker.sh
+++ install_docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
 sudo apt-get update
+sudo apt-get remove -y --purge man-db
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common tmux
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
