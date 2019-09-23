946e9be198f382ccaf584a87a42e2726fc4e3068
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-09-23 06:14:05 -0400

    Create nfsheadandstorage.sh

---
+++ nfsheadandstorage.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+apt-get install -y nfs-utils nfs-utils-lib
+#yum install -y nfs-utils nfs-utils-lib
+chkconfig nfs on
+service rpcbind start
+service nfs start
+mkdir /software
+mkdir /scratch
+
+cp /local/repository/source/* /scratch
+
+# Create the permissions file for the NFS directory.
+computes=$(($1 + 1))
+for i in $(seq $computes)
+do
+  st='/software 192.168.1.'
+  st+=$(($i))
+  st+='(rw,sync,no_root_squash,no_subtree_check)'
+  echo $st >> /etc/exports
+done
+
+#echo "/software 192.168.1.2(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.3(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.4(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.5(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.6(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.7(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.8(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.9(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.10(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.11(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.12(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.13(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+#echo "/software 192.168.1.14(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+
+exportfs -a
+echo "Done" >> /users/jk880380/headdoneNFS.txt
+
+set -x
+#sudo yum -y group install "Development Tools"
+sudo apt-get -y group install "Development Tools"
+cd /software
+sudo wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.2.tar.gz
+sudo tar xzf openmpi-3.1.2.tar.gz
+cd openmpi-3.1.2
+sudo ./configure --prefix=/software
+sudo make
+sudo make all install
+echo "export PATH='$PATH:/software/bin'" >> /users/jk880380/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/lib/'" >> /users/jk880380/.bashrc
+cd ..
+sudo rm -Rf openmpi-3.1.2
+sudo rm -Rf openmpi-3.1.2.tar.gz
+#mkdir /software/flagdir
+echo "Done" >> /users/jk880380/headdoneMPI.txt
+
+# Storage.
+
+touch /etc/exports
+touch /scratch/machine_list
+echo 'storage' >> /scratch/machine_list
+computes=$(($1 + 0))
+for i in $(seq $computes)
+do
+  st='/scratch 192.168.1.'
+  st+=$(($i + 2))
+  st+='(rw,sync,no_root_squash,no_subtree_check)'
+  echo $st >> /etc/exports
+  
+  st2='compute-'
+  st2+=$(($i + 0))
+  #st2='192.168.1.'
+  #st2+=$(($i + 2))
+  echo $st2 >> /scratch/machine_list
+done
+
+exportfs -a
+
+sleep 600
+#mkdir /software
+mount -t nfs 192.168.1.1:/software /software
+mkdir /software/flagdir
+echo "Done" >> /users/jk880380/storagedoneMPI.txt
