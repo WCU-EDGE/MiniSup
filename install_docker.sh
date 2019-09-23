649e7a5876c5b989128a58d9c1571dfabf16ab42
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-09-23 06:48:43 -0400

    tag-files

--- install_docker.sh
+++ install_docker.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 set -x
 sudo apt-get update
+echo "Done" >> /users/jk880380/dockerProcess01.txt
 sudo apt-get remove -y --purge man-db
+echo "Done" >> /users/jk880380/dockerProcess02.txt
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common tmux
+echo "Done" >> /users/jk880380/dockerProcess03.txt
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+echo "Done" >> /users/jk880380/dockerProcess04.txt
 sudo apt-get update
+echo "Done" >> /users/jk880380/dockerProcess05.txt
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
+echo "Done" >> /users/jk880380/dockerProcess06.txt
 sudo apt-get install -y httping
+echo "Done" >> /users/jk880380/dockerProcess07.txt
 sudo apt-get install -y jq
+echo "Done" >> /users/jk880380/dockerProcess08.txt
 
 # the username needs to be changed
 ##sudo usermod -aG docker lngo
 sudo usermod -aG docker jk880380
+echo "Done" >> /users/jk880380/dockerDone.txt
