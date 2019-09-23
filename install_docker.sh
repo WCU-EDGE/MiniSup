62f9670943dc8853bd3ed01af1be3e294dc79dc2
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-09-23 15:02:22 -0400

    break line up

--- install_docker.sh
+++ install_docker.sh
@@ -4,7 +4,18 @@ sudo apt-get update
 echo "Done" >> /users/jk880380/dockerProcess01.txt
 sudo apt-get remove -y --purge man-db
 echo "Done" >> /users/jk880380/dockerProcess02.txt
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common tmux
+sudo apt-get install -y apt-transport-https
+echo "Done" >> /users/jk880380/dockerProcess02A.txt
+sudo apt-get install -y ca-certificates
+echo "Done" >> /users/jk880380/dockerProcess02B.txt
+sudo apt-get install -y curl
+echo "Done" >> /users/jk880380/dockerProcess02C.txt
+sudo apt-get install -y gnupg-agent
+echo "Done" >> /users/jk880380/dockerProcess02D.txt
+sudo apt-get install -y software-properties-common
+echo "Done" >> /users/jk880380/dockerProcess02E.txt
+sudo apt-get install -y tmux
+#sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common tmux
 echo "Done" >> /users/jk880380/dockerProcess03.txt
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
