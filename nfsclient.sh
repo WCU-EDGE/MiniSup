1cf42e414c801fce7e9de11d1bc96384b8b6e21b
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-09-30 04:08:14 -0400

    Update nfsclient.sh

--- nfsclient.sh
+++ nfsclient.sh
@@ -1,7 +1,7 @@
 
 #!/bin/sh
 
-apt-get install -y nfs-utils nfs-utils-lib
+sudo apt-get install -y nfs-common
 #yum install -y nfs-utils nfs-utils-lib
 chkconfig nfs on
 service rpcbind start
@@ -9,13 +9,16 @@ service nfs start
 mkdir /software
 mkdir /scratch
 sleep 600
-mount -t nfs 192.168.1.1:/software /software
-mount -t nfs 192.168.1.2:/scratch /scratch
+sudo mount 192.168.1.1:/home /home
+sudo mount 192.168.1.1:/opt /opt
+sudo mount 192.168.1.1:/software /software
+#mount -t nfs 192.168.1.1:/software /software
+#mount -t nfs 192.168.1.2:/scratch /scratch
 
-while [ ! -d /software/flagdir ]
-do
-  sleep 30
-done
+#while [ ! -d /software/flagdir ]
+#do
+#  sleep 30
+#done
 
 echo "export PATH='$PATH:/software/bin'" >> /users/jk880380/.bashrc
 echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/lib/'" >> /users/jk880380/.bashrc
@@ -26,4 +29,4 @@ source /users/jk880380/.bashrc
 #  sleep 30
 #done
 
-#echo "Done" >> /users/jk880380/clientdone.txt
+echo "Done" >> ~/clientdone.txt
