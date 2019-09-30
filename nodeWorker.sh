93fd60bafbab8b6abfda5748dd0b2b8496c4c32e
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-09-30 07:10:50 -0400

    sudo debconf 3

--- nodeWorker.sh
+++ nodeWorker.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 
 # debconfs
-sudo echo "ldap-auth-config        ldap-auth-config/bindpw password" | debconf-set-selections
-sudo echo "ldap-auth-config        ldap-auth-config/rootbindpw     password" | debconf-set-selections
-sudo echo "libpam-runtime  libpam-runtime/profiles multiselect     unix, ldap, systemd, capability" | debconf-set-selections
-sudo echo "ldap-auth-config	ldap-auth-config/ldapns/ldap-server	string	ldap:///192.168.1.1" | debconf-set-selections
-sudo echo "ldap-auth-config	ldap-auth-config/ldapns/base-dn	string	dc=csc,dc=wcupa,dc=edu" | debconf-set-selections
-sudo echo "ldap-auth-config	ldap-auth-config/move-to-debconf	boolean	true" | debconf-set-selections
-sudo echo "ldap-auth-config	ldap-auth-config/rootbinddn	string	cn=admin,dc=csc,dc=wcupa,dc=edu" | debconf-set-selections
-sudo echo "ldap-auth-config	ldap-auth-config/dblogin	boolean	false" | debconf-set-selections
-sudo echo "ldap-auth-config	ldap-auth-config/pam_password	select	md5" | debconf-set-selections
-sudo echo "ldap-auth-config	ldap-auth-config/dbrootlogin	boolean	true" | debconf-set-selections
-sudo echo "ldap-auth-config	ldap-auth-config/ldapns/ldap_version	select	3" | debconf-set-selections
-sudo echo "ldap-auth-config	ldap-auth-config/binddn	string	cn=proxyuser,dc=example,dc=net" | debconf-set-selections
-sudo echo "ldap-auth-config	ldap-auth-config/override	boolean	true" | debconf-set-selections
-#sudo echo "libpam0g	libpam0g/restart-failed	error	" | debconf-set-selections
-#sudo echo "libpam0g:amd64	libpam0g/restart-failed	error	" | debconf-set-selections
-#sudo echo "libpam-runtime	libpam-runtime/conflicts	error	" | debconf-set-selections
-#sudo echo "libpam0g	libpam0g/xdm-needs-restart	error	" | debconf-set-selections
-#sudo echo "libpam0g:amd64	libpam0g/xdm-needs-restart	error	" | debconf-set-selections
-#sudo echo "libpam0g	libraries/restart-without-asking	boolean	false" | debconf-set-selections
-#sudo echo "libpam0g:amd64	libraries/restart-without-asking	boolean	false" | debconf-set-selections
-#sudo echo "libpam0g	libpam0g/restart-services	string	" | debconf-set-selections
-#sudo echo "libpam0g:amd64	libpam0g/restart-services	string	" | debconf-set-selections
-#sudo echo "libpam-modules	libpam-modules/disable-screensaver	error	" | debconf-set-selections
-#sudo echo "libpam-runtime	libpam-runtime/profiles	multiselect	unix, ldap, systemd, capability" | debconf-set-selections
-#sudo echo "libpam-runtime	libpam-runtime/no_profiles_chosen	error" | debconf-set-selections
-#sudo echo "libpam-runtime	libpam-runtime/override	boolean	false" | debconf-set-selections
+sudo echo "ldap-auth-config        ldap-auth-config/bindpw password" | sudo debconf-set-selections
+sudo echo "ldap-auth-config        ldap-auth-config/rootbindpw     password" | sudo debconf-set-selections
+sudo echo "libpam-runtime  libpam-runtime/profiles multiselect     unix, ldap, systemd, capability" | sudo debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/ldapns/ldap-server	string	ldap:///192.168.1.1" | sudo debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/ldapns/base-dn	string	dc=csc,dc=wcupa,dc=edu" | sudo debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/move-to-debconf	boolean	true" | sudo debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/rootbinddn	string	cn=admin,dc=csc,dc=wcupa,dc=edu" | sudo debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/dblogin	boolean	false" | sudo debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/pam_password	select	md5" | sudo debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/dbrootlogin	boolean	true" | sudo debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/ldapns/ldap_version	select	3" | sudo debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/binddn	string	cn=proxyuser,dc=example,dc=net" | sudo debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/override	boolean	true" | sudo debconf-set-selections
+#sudo echo "libpam0g	libpam0g/restart-failed	error	" | sudo debconf-set-selections
+#sudo echo "libpam0g:amd64	libpam0g/restart-failed	error	" | sudo debconf-set-selections
+#sudo echo "libpam-runtime	libpam-runtime/conflicts	error	" | sudo debconf-set-selections
+#sudo echo "libpam0g	libpam0g/xdm-needs-restart	error	" | sudo debconf-set-selections
+#sudo echo "libpam0g:amd64	libpam0g/xdm-needs-restart	error	" | sudo debconf-set-selections
+#sudo echo "libpam0g	libraries/restart-without-asking	boolean	false" | sudo debconf-set-selections
+#sudo echo "libpam0g:amd64	libraries/restart-without-asking	boolean	false" | sudo debconf-set-selections
+#sudo echo "libpam0g	libpam0g/restart-services	string	" | sudo debconf-set-selections
+#sudo echo "libpam0g:amd64	libpam0g/restart-services	string	" | sudo debconf-set-selections
+#sudo echo "libpam-modules	libpam-modules/disable-screensaver	error	" | sudo debconf-set-selections
+#sudo echo "libpam-runtime	libpam-runtime/profiles	multiselect	unix, ldap, systemd, capability" | sudo debconf-set-selections
+#sudo echo "libpam-runtime	libpam-runtime/no_profiles_chosen	error" | sudo debconf-set-selections
+#sudo echo "libpam-runtime	libpam-runtime/override	boolean	false" | sudo debconf-set-selections
 
 sudo apt-get update
 sudo apt-get install -y libnss-ldap libpam-ldap ldap-utils
