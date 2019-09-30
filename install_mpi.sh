6208f2034c99206a67771844854c4e5ca68ad006
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-09-30 02:15:54 -0400

    Create install_mpi.sh

---
+++ install_mpi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+sudo yum -y group install "Development Tools"
+sudo wget https://download.open-mpi.org/release/open-mpi/v3.1/openmpi-3.1.2.tar.gz
+sudo tar xzf openmpi-3.1.2.tar.gz
+cd openmpi-3.1.2
+sudo ./configure --prefix=/opt/openmpi/3.1.2 --enable-mpirun-prefix-by-default
+sudo make
+sudo make all install
+
+# change to your username instead lngo (my username)
+echo "export PATH=$PATH:/opt/openmpi/3.1.2/bin" >> /users/jk880380/.bashrc
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/openmpi/3.1.2/lib/" >> /users/jk880380/.bashrc
+cd ..
+sudo rm -Rf openmpi-3.1.2
+sudo rm -Rf openmpi-3.1.2.tar.gz 
