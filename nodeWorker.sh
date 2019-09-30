7a6a2a4222071fcb8b2560a48d4a176213a7125f
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-09-30 04:49:59 -0400

    Update nodeWorker.sh

--- nodeWorker.sh
+++ nodeWorker.sh
@@ -1,16 +1,49 @@
 #!/bin/sh
 
+sudo echo "ldap-auth-config	ldap-auth-config/ldapns/ldap-server	string	ldap:///192.168.1.1" | debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/ldapns/base-dn	string	dc=csc,dc=wcupa,dc=edu" | debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/move-to-debconf	boolean	true" | debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/rootbinddn	string	cn=admin,dc=csc,dc=wcupa,dc=edu" | debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/dblogin	boolean	false" | debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/pam_password	select	md5" | debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/dbrootlogin	boolean	true" | debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/ldapns/ldap_version	select	3" | debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/binddn	string	cn=proxyuser,dc=example,dc=net" | debconf-set-selections
+sudo echo "ldap-auth-config	ldap-auth-config/override	boolean	true" | debconf-set-selections
+#sudo echo "libpam0g	libpam0g/restart-failed	error	" | debconf-set-selections
+#sudo echo "libpam0g:amd64	libpam0g/restart-failed	error	" | debconf-set-selections
+#sudo echo "libpam-runtime	libpam-runtime/conflicts	error	" | debconf-set-selections
+#sudo echo "libpam0g	libpam0g/xdm-needs-restart	error	" | debconf-set-selections
+#sudo echo "libpam0g:amd64	libpam0g/xdm-needs-restart	error	" | debconf-set-selections
+#sudo echo "libpam0g	libraries/restart-without-asking	boolean	false" | debconf-set-selections
+#sudo echo "libpam0g:amd64	libraries/restart-without-asking	boolean	false" | debconf-set-selections
+#sudo echo "libpam0g	libpam0g/restart-services	string	" | debconf-set-selections
+#sudo echo "libpam0g:amd64	libpam0g/restart-services	string	" | debconf-set-selections
+#sudo echo "libpam-modules	libpam-modules/disable-screensaver	error	" | debconf-set-selections
+#sudo echo "libpam-runtime	libpam-runtime/profiles	multiselect	unix, ldap, systemd, capability" | debconf-set-selections
+#sudo echo "libpam-runtime	libpam-runtime/no_profiles_chosen	error" | debconf-set-selections
+#sudo echo "libpam-runtime	libpam-runtime/override	boolean	false" | debconf-set-selections
+
+sudo apt-get update
+sudo apt-get install -y libnss-ldap libpam-ldap ldap-utils
+
+sudo sed -i 's/compat systemd/compat systemd ldap/g' /etc/nsswitch.conf
+sudo sed -i 's/use_authtok//g' /etc/pam.d/common-password
+echo 'session optional pam_mkhomedir.so skel=/etc/skel umask=077' | sudo tee -a /etc/pam.d/common-session
+
 sudo apt-get install -y nfs-common
-#yum install -y nfs-utils nfs-utils-lib
-chkconfig nfs on
-service rpcbind start
-service nfs start
-mkdir /software
-mkdir /scratch
+
+#chkconfig nfs on
+#service rpcbind start
+#service nfs start
+
+#mkdir /software
+#mkdir /scratch
 sleep 600
 sudo mount 192.168.1.1:/home /home
 sudo mount 192.168.1.1:/opt /opt
 sudo mount 192.168.1.1:/software /software
+sudo mount 192.168.1.1:/scratch /scratch
 #mount -t nfs 192.168.1.1:/software /software
 #mount -t nfs 192.168.1.2:/scratch /scratch
 
@@ -19,8 +52,8 @@ sudo mount 192.168.1.1:/software /software
 #  sleep 30
 #done
 
-echo "export PATH='$PATH:/software/bin'" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/lib/'" >> ~/.bashrc
+echo "export PATH='$PATH:/software/bin'" >> /users/jk880380/.bashrc
+echo "export LD_LIBRARY_PATH='$LD_LIBRARY_PATH:/software/lib/'" >> /users/jk880380/.bashrc
 source ~/.bashrc
 
 #while [ ! -d /scratch/flagdir ]
@@ -28,4 +61,4 @@ source ~/.bashrc
 #  sleep 30
 #done
 
-echo "Done" >> ~/clientdone.txt
+echo "Done" >> /users/jk880380/clientdone.txt
