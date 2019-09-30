a4a668edffdef1ccfbdd17f829d1f8754fb48777
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-09-30 03:56:35 -0400

    debconf

--- install_docker.sh
+++ install_docker.sh
@@ -1,33 +1,28 @@
 #!/bin/bash
 set -x
+
+# debconf
+sudo echo "libssl1.0.0	libssl1.0.0/restart-failed	error" | debconf-set-selections
+sudo echo "libssl1.0.0:amd64	libssl1.0.0/restart-failed	error	" | debconf-set-selections
+sudo echo "libssl1.1	libraries/restart-without-asking	boolean	false" | debconf-set-selections
+sudo echo "libssl1.1:amd64	libraries/restart-without-asking	boolean	false" | debconf-set-selections
+sudo echo "libssl1.0.0	libssl1.0.0/restart-services	string	" | debconf-set-selections
+sudo echo "libssl1.0.0:amd64	libssl1.0.0/restart-services	string	" | debconf-set-selections
+sudo echo "libssl1.1	libssl1.1/restart-failed	error	" | debconf-set-selections
+sudo echo "libssl1.1:amd64	libssl1.1/restart-failed	error	" | debconf-set-selections
+sudo echo "libssl1.1	libssl1.1/restart-services	string	" | debconf-set-selections
+sudo echo "libssl1.1:amd64	libssl1.1/restart-services	string	" | debconf-set-selections
+
 sudo apt-get update
-echo "Done" >> /users/jk880380/dockerProcess01.txt
-sudo apt-get remove -y --purge man-db
-echo "Done" >> /users/jk880380/dockerProcess02.txt
-sudo apt-get install -y apt-transport-https
-echo "Done" >> /users/jk880380/dockerProcess02A.txt
-sudo apt-get install -y ca-certificates
-echo "Done" >> /users/jk880380/dockerProcess02B.txt
-sudo apt-get install -y curl
-echo "Done" >> /users/jk880380/dockerProcess02C.txt
-sudo apt-get install -y gnupg-agent
-echo "Done" >> /users/jk880380/dockerProcess02D.txt
-sudo apt-get install -y software-properties-common
-echo "Done" >> /users/jk880380/dockerProcess02E.txt
-sudo apt-get install -y tmux
-#sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common tmux
-echo "Done" >> /users/jk880380/dockerProcess03.txt
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common tmux
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-echo "Done" >> /users/jk880380/dockerProcess04.txt
+
 sudo apt-get update
-echo "Done" >> /users/jk880380/dockerProcess05.txt
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
-echo "Done" >> /users/jk880380/dockerProcess06.txt
 sudo apt-get install -y httping
-echo "Done" >> /users/jk880380/dockerProcess07.txt
 sudo apt-get install -y jq
-echo "Done" >> /users/jk880380/dockerProcess08.txt
+
 
 # the username needs to be changed
 ##sudo usermod -aG docker lngo
