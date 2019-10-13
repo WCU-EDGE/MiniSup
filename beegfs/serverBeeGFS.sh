794a6c91e49fd5c68f2a8f53c165e0bd5cc9aad0
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-10-13 06:46:42 -0400

    remove errant install

--- beegfs/serverBeeGFS.sh
+++ beegfs/serverBeeGFS.sh
@@ -5,7 +5,7 @@
 sudo wget -O /etc/apt/sources.list.d/beegfs-deb9.list https://www.beegfs.io/release/latest-stable/dists/beegfs-deb9.list
 sudo wget -q https://www.beegfs.io/release/latest-stable/gpg/DEB-GPG-KEY-beegfs -O- | sudo apt-key add -
 sudo apt-get update -y
-sudo apt-get install -y install beegfs-mgmtd
+sudo apt-get install -y beegfs-mgmtd
 sudo apt-get install -y beegfs-meta
 sudo apt-get install -y beegfs-storage
 sudo apt-get install -y beegfs-client beegfs-helperd beegfs-utils
