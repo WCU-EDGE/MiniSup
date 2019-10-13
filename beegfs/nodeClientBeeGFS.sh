063850a201accab91f7492b0cd8c9f2dd2c59c29
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-10-13 06:01:05 -0400

    Create nodeClientBeeGFS.sh

---
+++ beegfs/nodeClientBeeGFS.sh
@@ -0,0 +1,10 @@
+# https://www.beegfs.io/wiki/ManualInstallWalkThrough
+
+sudo wget -O /etc/apt/sources.list.d/beegfs-deb9.list https://www.beegfs.io/release/latest-stable/dists/beegfs-deb9.list
+sudo wget -q https://www.beegfs.io/release/latest-stable/gpg/DEB-GPG-KEY-beegfs -O- | sudo apt-key add -
+sudo apt-get update -y
+sudo apt-get install -y beegfs-client beegfs-helperd beegfs-utils
+
+/opt/beegfs/sbin/beegfs-setup-client -m beenode
+sudo systemctl start beegfs-helperd
+sudo systemctl start beegfs-client
