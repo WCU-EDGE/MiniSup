737e627e5895cc345999298ccbb22a0fa7707b1b
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-10-20 17:16:02 -0400

    users.ldif is in /local/repository

--- nodeHead.sh
+++ nodeHead.sh
@@ -36,7 +36,7 @@ sudo ufw allow ldap
 
 sudo /local/repository/ldif/createUsersLdif.sh
 sudo ldapadd -x -D cn=admin,dc=csc,dc=wcupa,dc=edu -W -f /local/repository/ldif/basedln.ldif
-sudo ldapadd -x -D cn=admin,dc=csc,dc=wcupa,dc=edu -W -f /local/repository/ldif/users.ldif
+sudo ldapadd -x -D cn=admin,dc=csc,dc=wcupa,dc=edu -W -f /local/repository/users.ldif
 
 sudo apt-get install -y nfs-kernel-server
 #apt-get install -y nfs-utils nfs-utils-lib
