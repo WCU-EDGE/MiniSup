1d6e96284365b63ca25a01d5db40082bc20f1df8
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-10-27 22:04:07 -0400

    remove pw

--- nodeHead.sh
+++ nodeHead.sh
@@ -26,12 +26,12 @@ echo 'debconf start'
 export DEBIAN_FRONTEND=noninteractive
 #sudo sh -c 'cat /local/repository/preseedHead.deb | debconf-set-selections'
 cat /local/repository/preseedHead.deb | sudo debconf-set-selections
-sudo debconf-set-selections <<< 'slapd slapd/root_password password admin'
-sudo debconf-set-selections <<< 'slapd slapd/root_password_again password admin'
-sudo debconf-set-selections <<< 'slapd slapd/internal/generated_adminpw password admin'
-sudo debconf-set-selections <<< 'slapd slapd/internal/adminpw password admin'
-sudo debconf-set-selections <<< 'slapd slapd/password1 password admin'
-sudo debconf-set-selections <<< 'slapd slapd/password2 password admin'
+#sudo debconf-set-selections <<< 'slapd slapd/root_password password admin'
+#sudo debconf-set-selections <<< 'slapd slapd/root_password_again password admin'
+#sudo debconf-set-selections <<< 'slapd slapd/internal/generated_adminpw password admin'
+#sudo debconf-set-selections <<< 'slapd slapd/internal/adminpw password admin'
+#sudo debconf-set-selections <<< 'slapd slapd/password1 password admin'
+#sudo debconf-set-selections <<< 'slapd slapd/password2 password admin'
 echo 'debconf end'
 
 #sudo DEBIAN_FRONTEND=noninteractive apt-get install -y slapd ldap-utils
