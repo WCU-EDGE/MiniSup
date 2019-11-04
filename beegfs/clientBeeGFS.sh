28333a38297cd738ac4dd91ba838344896e3e4c1
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-03 22:33:11 -0500

    Copy beegfs apt file from local repository

--- beegfs/clientBeeGFS.sh
+++ beegfs/clientBeeGFS.sh
@@ -6,8 +6,10 @@ echo 'clientBeeGFS.sh'
 
 #sudo wget -O /etc/apt/sources.list.d/beegfs-deb9.list https://www.beegfs.io/release/latest-stable/dists/beegfs-deb9.list
 
-# The next line is the contents of the beegfs-deb8.list file from beegfs.io, which isn't always available for some reason.
-sudo sh -c "echo 'deb [arch=amd64] https://www.beegfs.io/release/beegfs_7_1 jessie non-free' >> /etc/apt/sources.list.d/beegfs-deb8.list"
+## The next line is the contents of the beegfs-deb8.list file from beegfs.io, which isn't always available for some reason.
+#sudo sh -c "echo 'deb [arch=amd64] https://www.beegfs.io/release/beegfs_7_1 jessie non-free' >> /etc/apt/sources.list.d/beegfs-deb8.list"
+
+sudo cp /local/repository/beegfs/beegfs-deb8.list /etc/apt/sources.list.d/beegfs-deb8.list
 
 sudo wget -q https://www.beegfs.io/release/latest-stable/gpg/DEB-GPG-KEY-beegfs -O- | sudo apt-key add -
 sudo apt-get update -y
