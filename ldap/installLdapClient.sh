fea71e3a2c05300105c0b7fdd289b63e60ecef21
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-12 04:44:00 -0500

    manually copy /etc/ldap.conf

--- ldap/installLdapClient.sh
+++ ldap/installLdapClient.sh
@@ -16,6 +16,8 @@ echo 'debconf end'
 sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure ldap-auth-config
 sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure libpam-runtime
 
+sudo cp -rf /local/repository/ldap/ldap.conf.client /etc/ldap.conf
+
 sudo sed -i 's/compat systemd/compat systemd ldap/g' /etc/nsswitch.conf
 sudo sed -i 's/use_authtok//g' /etc/pam.d/common-password
 echo 'session optional pam_mkhomedir.so skel=/etc/skel umask=077' | sudo tee -a /etc/pam.d/common-session
