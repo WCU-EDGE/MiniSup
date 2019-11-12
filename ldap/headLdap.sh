a54b0479572750834a588fb151e40ffb1a976f24
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-12 03:16:57 -0500

    remove commented-out instructions ldapHead.sh

--- ldap/headLdap.sh
+++ ldap/headLdap.sh
@@ -4,29 +4,15 @@ set -x
 
 ## Install LDAP head node
 
+# Installation
 sudo apt-get update
 sudo apt-get install -y debconf-utils
-
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y slapd ldap-utils
 
-# debconf
-echo 'debconf start'
+# debconf preseeding (must do AFTER the install or some fields get overwritten)
 export DEBIAN_FRONTEND=noninteractive
-#sudo sh -c 'cat /local/repository/preseedHead.deb | debconf-set-selections'
-cat /local/repository/preseedHead.deb | sudo debconf-set-selections
-#sudo debconf-set-selections <<< 'slapd slapd/root_password password admin'
-#sudo debconf-set-selections <<< 'slapd slapd/root_password_again password admin'
-#sudo debconf-set-selections <<< 'slapd slapd/internal/generated_adminpw password admin'
-#sudo debconf-set-selections <<< 'slapd slapd/internal/adminpw password admin'
-#sudo debconf-set-selections <<< 'slapd slapd/password1 password admin'
-#sudo debconf-set-selections <<< 'slapd slapd/password2 password admin'
-echo 'debconf end'
+cat /local/repository/ldap/preseedHead.deb | sudo debconf-set-selections
 
-#sudo DEBIAN_FRONTEND=noninteractive apt-get install -y slapd ldap-utils
-
-#sudo /local/repository/ldap/configLdap.sh
-
-
-#sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure slapd
+# Reconfigure
 sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure slapd
 sudo ufw allow ldap
