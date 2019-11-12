534a1766875e68a381e1916b614c1726fab127b1
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-12 06:22:35 -0500

    look for flagdir during nfs mounts

--- nodeWorker.sh
+++ nodeWorker.sh
@@ -28,16 +28,27 @@ sudo apt-get install -y nfs-common
 ###mount -t nfs 192.168.1.1:/software /software
 ###mount -t nfs 192.168.1.2:/scratch /scratch
 
+#sudo mkdir /opt/shared
+#sudo mkdir /software
+#sudo mount 192.168.1.1:/home /home || true
+## Cycle until we can mount home.
+#while [ mount | grep home > /dev/null ]; do
+#  sudo mount 192.168.1.1:/home /home || true
+#  sleep 60
+#done
+#sudo mount 192.168.1.1:/opt /opt/shared
+#sudo mount 192.168.1.1:/software /software
+
 sudo mkdir /opt/shared
 sudo mkdir /software
-sudo mount 192.168.1.1:/home /home || true
-# Cycle until we can mount home.
-while [ mount | grep home > /dev/null ]; do
-  sudo mount 192.168.1.1:/home /home || true
+sudo mount 192.168.1.1:/software /software || true
+# Cycle until we can mount software.
+while [ ! -d /software/flagdir ]; do
+  sudo mount 192.168.1.1:/software /software || true
   sleep 60
 done
+sudo mount 192.168.1.1:/home /home
 sudo mount 192.168.1.1:/opt /opt/shared
-sudo mount 192.168.1.1:/software /software
 
 #while [ ! -d /software/flagdir ]
 #do
