b7074f821df2a35afb1c1d299b4af1c81dec3b1a
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-12 04:28:11 -0500

    move ldap client install to its own sh

--- nodeWorker.sh
+++ nodeWorker.sh
@@ -8,22 +8,8 @@ echo 'nodeWorker.sh'
 export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y libnss-ldap libpam-ldap ldap-utils
 
-echo 'debconf start'
-export DEBIAN_FRONTEND=noninteractive
-cat /local/repository/preseedWorker.deb | sudo debconf-set-selections
-echo 'debconf end'
-
-#sudo dpkg-reconfigure slapd
-sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure ldap-auth-config
-sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure libpam-runtime
-
-sudo sed -i 's/compat systemd/compat systemd ldap/g' /etc/nsswitch.conf
-sudo sed -i 's/use_authtok//g' /etc/pam.d/common-password
-echo 'session optional pam_mkhomedir.so skel=/etc/skel umask=077' | sudo tee -a /etc/pam.d/common-session
-
-sudo ufw allow ldap
+sudo /local/repository/ldap/clientLdap.sh
 
 sudo apt-get install -y nfs-common
 
