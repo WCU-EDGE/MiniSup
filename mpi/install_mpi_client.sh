733b8ce03a87f09802cac57f7f9f9615db9a9e7d
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-13 06:50:33 -0500

    source ~/.bashrc to install_mpi_client

--- mpi/install_mpi_client.sh
+++ mpi/install_mpi_client.sh
@@ -6,3 +6,4 @@ echo "install_mpi_client.sh"
 
 echo "export PATH=$PATH:/opt/shared/openmpi/3.1.2/bin" >> /users/$USER/.bashrc
 echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/shared/openmpi/3.1.2/lib/" >> /users/$USER/.bashrc
+source ~/.bashrc
