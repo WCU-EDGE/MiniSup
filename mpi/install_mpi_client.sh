c22d11d1730c3feaf66cef2ef3f3879a53b52b06
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-13 23:53:29 -0500

    immediate path setting

--- mpi/install_mpi_client.sh
+++ mpi/install_mpi_client.sh
@@ -12,3 +12,7 @@ echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/shared/openmpi/3.1.2/lib/' | 
 # Guarantee LD_LIBRARY_PATH isn't empty, so we don't have a leading colon in it during a later export (which
 #  would lead to a security hole where Linux first searches the current directory for libraries!)
 echo 'export LD_LIBRARY_PATH=/opt/shared/openmpi/3.1.2/lib/' | sudo tee -a /etc/environment
+
+# Set path right now.
+sudo export PATH=$PATH:/opt/shared/openmpi/3.1.2/bin
+sudo export LD_LIBRARY_PATH=/opt/shared/openmpi/3.1.2/lib/
