ede2a1dce43d34e89e23a1f49e8aa04ed152be42
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-13 21:06:52 -0500

    make echo single-quote so can have more than one for different packages

--- mpi/install_mpi_client.sh
+++ mpi/install_mpi_client.sh
@@ -4,11 +4,11 @@ set -x
 
 echo "install_mpi_client.sh"
 
-#echo "export PATH=$PATH:/opt/shared/openmpi/3.1.2/bin" >> /users/$USER/.bashrc
-#echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/shared/openmpi/3.1.2/lib/" >> /users/$USER/.bashrc
-#source ~/.bashrc
+# We run MPI using the network installation, so we only need to add that to our path(s). Make sure all users
+#  have this in their path(s).
+echo 'export PATH=$PATH:/opt/shared/openmpi/3.1.2/bin' | sudo tee -a /etc/skel/.bashrc
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/shared/openmpi/3.1.2/lib/' | sudo tee -a /etc/skel/.bashrc
 
-# We run MPI using the network installation, so we only need to add that to our path(s)
-echo "export PATH=$PATH:/opt/shared/openmpi/3.1.2/bin" >> /etc/skel/.bashrc
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/shared/openmpi/3.1.2/lib/" >> /etc/skel/.bashrc
-source ~/.bashrc
+# Guarantee LD_LIBRARY_PATH isn't empty, so we don't have a leading colon in it during a later export (which
+#  would lead to a security hole where Linux first searches the current directory for libraries!)
+echo 'export LD_LIBRARY_PATH=/opt/shared/openmpi/3.1.2/lib/' | sudo tee -a /etc/environment
