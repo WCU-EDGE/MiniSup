8afa1a6d6cde7c3c209382f83fffc8e51f636eeb
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-13 21:18:00 -0500

    Remove old code

--- nfs/installNfsHead.sh
+++ nfs/installNfsHead.sh
@@ -5,22 +5,16 @@ set -x
 echo 'installNfsHead.sh'
 
 mkdir /software
-#mkdir /scratch
 sudo chown nobody:nogroup /software
-#sudo chown nobody:nogroup /scratch
 sudo chown nobody:nogroup /home
 
 sudo chmod -R a+rx /software
-#sudo chmod -R 777 /scratch
 sudo chmod -R 777 /home
 sudo chmod -R a+rx /opt
-
 
 #################
 
 sudo apt-get install -y nfs-kernel-server
-#apt-get install -y nfs-utils nfs-utils-lib
-#yum install -y nfs-utils nfs-utils-lib
 
 # Create the permissions file for the NFS directory.
 computes=$(($1 + 1))
@@ -43,44 +37,3 @@ sudo systemctl restart nfs-kernel-server
 #chkconfig nfs on
 #service rpcbind start
 #service nfs start
-
-
-## Copy, if exists.
-#cp /local/repository/source/* /scratch || true
-
-## Create the permissions file for the NFS directory.
-#computes=$(($1 + 1))
-#for i in $(seq $computes)
-#do
-#  st='/software 192.168.1.'
-#  st+=$(($i))
-#  st+='(rw,sync,no_root_squash,no_subtree_check)'
-#  echo $st >> /etc/exports
-#done
-
-#exportfs -a
-#echo "Done" >> /users/jk880380/headdoneNFS.txt
-
-
-
-# Storage.
-
-#touch /etc/exports
-#touch /scratch/machine_list
-#echo 'storage' >> /scratch/machine_list
-#computes=$(($1 + 0))
-#for i in $(seq $computes)
-#do
-#  st='/scratch 192.168.1.'
-#  st+=$(($i + 2))
-#  st+='(rw,sync,no_root_squash,no_subtree_check)'
-#  echo $st >> /etc/exports
-#  
-#  st2='compute-'
-#  st2+=$(($i + 0))
-#  #st2='192.168.1.'
-#  #st2+=$(($i + 2))
-#  echo $st2 >> /scratch/machine_list
-#done
-#
-#exportfs -a
