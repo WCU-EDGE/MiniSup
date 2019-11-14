f0a188e45256501507d3069d5f606e3351d1c88a
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-13 20:53:00 -0500

    update bashrc in /etc/skel for new users

--- mpi/install_mpi_client.sh
+++ mpi/install_mpi_client.sh
@@ -4,6 +4,11 @@ set -x
 
 echo "install_mpi_client.sh"
 
-echo "export PATH=$PATH:/opt/shared/openmpi/3.1.2/bin" >> /users/$USER/.bashrc
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/shared/openmpi/3.1.2/lib/" >> /users/$USER/.bashrc
+#echo "export PATH=$PATH:/opt/shared/openmpi/3.1.2/bin" >> /users/$USER/.bashrc
+#echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/shared/openmpi/3.1.2/lib/" >> /users/$USER/.bashrc
+#source ~/.bashrc
+
+# We run MPI using the network installation, so we only need to add that to our path(s)
+echo "export PATH=$PATH:/opt/shared/openmpi/3.1.2/bin" >> /etc/skel/.bashrc
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/shared/openmpi/3.1.2/lib/" >> /etc/skel/.bashrc
 source ~/.bashrc
