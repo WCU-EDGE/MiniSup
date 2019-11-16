ab8fad828b51aaf3240f8584eaeef0924730d3ef
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-15 23:38:42 -0500

    /opt/shared -> /software

--- mpi/install_mpi_client.sh
+++ mpi/install_mpi_client.sh
@@ -6,13 +6,14 @@ echo "install_mpi_client.sh"
 
 # We run MPI using the network installation, so we only need to add that to our path(s). Make sure all users
 #  have this in their path(s).
-echo 'export PATH=$PATH:/opt/shared/openmpi/3.1.2/bin' | sudo tee -a /etc/skel/.bashrc
-echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/shared/openmpi/3.1.2/lib/' | sudo tee -a /etc/skel/.bashrc
+echo 'export PATH=$PATH:/software/openmpi/3.1.2/bin' | sudo tee -a /etc/skel/.bashrc
+#echo 'export PATH=$PATH:/opt/shared/openmpi/3.1.2/bin' | sudo tee -a /etc/skel/.bashrc
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/' | sudo tee -a /etc/skel/.bashrc
 
 # Add openmpi bin and library to path for everyone.
-sudo sed -i '/^PATH/ s/\"$/\:\/opt\/shared\/openmpi\/3.1.2\/bin\"/g' /etc/environment
-echo 'LD_LIBRARY_PATH=/opt/shared/openmpi/3.1.2/lib/' | sudo tee -a /etc/environment
+sudo sed -i '/^PATH/ s/\"$/\:\/software\/openmpi\/3.1.2\/bin\"/g' /etc/environment
+echo 'LD_LIBRARY_PATH=/software/openmpi/3.1.2/lib/' | sudo tee -a /etc/environment
 
 # Set path right now.
-export PATH=$PATH:/opt/shared/openmpi/3.1.2/bin
-export LD_LIBRARY_PATH=/opt/shared/openmpi/3.1.2/lib/
+export PATH=$PATH:/software/openmpi/3.1.2/bin
+export LD_LIBRARY_PATH=/software/openmpi/3.1.2/lib/
