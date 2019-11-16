008edf89b1639e574cd40ae4223e357258fc87d5
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-16 02:39:09 -0500

    add mpishare and fstab

--- nfs/installNfsClient.sh
+++ nfs/installNfsClient.sh
@@ -8,34 +8,10 @@ sudo apt-get update
 
 sudo apt-get install -y nfs-common
 
-#chkconfig nfs on
-#service rpcbind start
-#service nfs start
-
-##mkdir /scratch
-#sleep 600
-#sudo mkdir /opt/shared
-#sudo mkdir /software
-#sudo mount 192.168.1.1:/home /home
-#sudo mount 192.168.1.1:/opt /opt/shared
-#sudo mount 192.168.1.1:/software /software
-##sudo mount 192.168.1.1:/scratch /scratch
-###mount -t nfs 192.168.1.1:/software /software
-###mount -t nfs 192.168.1.2:/scratch /scratch
-
-#sudo mkdir /opt/shared
-#sudo mkdir /software
-#sudo mount 192.168.1.1:/home /home || true
-## Cycle until we can mount home.
-#while [ mount | grep home > /dev/null ]; do
-#  sudo mount 192.168.1.1:/home /home || true
-#  sleep 60
-#done
-#sudo mount 192.168.1.1:/opt /opt/shared
-#sudo mount 192.168.1.1:/software /software
-
+sudo mkdir /mpishare
 sudo mkdir /opt/shared
 sudo mkdir /software
+
 sudo mount 192.168.1.1:/software /software || true
 # Cycle until we can mount software.
 while [ ! -d /software/flagdir ]; do
@@ -43,4 +19,11 @@ while [ ! -d /software/flagdir ]; do
   sleep 60
 done
 sudo mount 192.168.1.1:/home /home
+sudo mount 192.168.1.1:/mpishare /mpishare
 sudo mount 192.168.1.1:/opt /opt/shared
+
+# Keep the shared dirs after a reboot
+echo '192.168.1.1:/home /home nfs' | sudo tee -a /etc/fstab
+echo '192.168.1.1:/mpishare /mpishare nfs' | sudo tee -a /etc/fstab
+echo '192.168.1.1:/opt /opt/shared nfs' | sudo tee -a /etc/fstab
+echo '192.168.1.1:/software /software nfs' | sudo tee -a /etc/fstab
