88dfee8cf3211997286d4db72b4f3a86481a5115
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-16 02:34:31 -0500

    add mpishare

--- nfs/installNfsHead.sh
+++ nfs/installNfsHead.sh
@@ -4,13 +4,18 @@ set -x
 
 echo 'installNfsHead.sh'
 
+mkdir /mpishare
 mkdir /software
-sudo chown nobody:nogroup /software
-sudo chown nobody:nogroup /home
 
-sudo chmod -R a+rx /software
+sudo chown nobody:nogroup /home
+sudo chown nobody:nogroup /mpishare
+sudo chown nobody:nogroup /opt
+sudo chown nobody:nogroup /software
+
 sudo chmod -R 777 /home
+sudo chmod -R a+rx /mpishare
 sudo chmod -R a+rx /opt
+sudo chmod -R a+rx /software
 
 #################
 
@@ -21,19 +26,16 @@ computes=$(($1 + 1))
 for i in $(seq 2 $computes)
 do
   echo "/home 192.168.1.$i(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
+  echo "/mpishare 192.168.1.$i(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
   echo "/opt 192.168.1.$i(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
   echo "/software 192.168.1.$i(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
-  #echo "/scratch 192.168.1.$i(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 done
 
 # Let the Slurm node have access to the shared directories.
 slurmip=$(($2 + 1))
 echo "/home 192.168.1.$slurmip(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
+echo "/mpishare 192.168.1.$slurmip(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 echo "/opt 192.168.1.$slurmip(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 echo "/software 192.168.1.$slurmip(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 
 sudo systemctl restart nfs-kernel-server
-
-#chkconfig nfs on
-#service rpcbind start
-#service nfs start
