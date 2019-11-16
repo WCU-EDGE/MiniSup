0180a458246ef7e6843b3f12e68f6cc1445d4582
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-16 02:49:22 -0500

    new MPI client that WORKS on clean install

--- mpi/install_mpi_client.sh
+++ mpi/install_mpi_client.sh
@@ -4,16 +4,36 @@ set -x
 
 echo "install_mpi_client.sh"
 
-# We run MPI using the network installation, so we only need to add that to our path(s). Make sure all users
-#  have this in their path(s).
-echo 'export PATH=$PATH:/software/openmpi/3.1.2/bin' | sudo tee -a /etc/skel/.bashrc
-#echo 'export PATH=$PATH:/opt/shared/openmpi/3.1.2/bin' | sudo tee -a /etc/skel/.bashrc
-echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/' | sudo tee -a /etc/skel/.bashrc
+PATH=$PATH:/software/mpiexec/bin
 
-# Add openmpi bin and library to path for everyone.
-sudo sed -i '/^PATH/ s/\"$/\:\/software\/openmpi\/3.1.2\/bin\"/g' /etc/environment
-echo 'LD_LIBRARY_PATH=/software/openmpi/3.1.2/lib/' | sudo tee -a /etc/environment
+# We run MPI using the network installation, so we need to add that to our path(s). Make sure all users
+#  have this in their path(s).
+echo 'export PATH=$PATH:/software/mpiexec/bin' | sudo tee -a /etc/skel/.bashrc
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/mpiexec/lib/' | sudo tee -a /etc/skel/.bashrc
+
+# Everyone should look to the openmpi binaries.
+sudo sed -i '/^PATH/ s/\"$/\:\/software\/mpiexec\/bin\"/g' /etc/environment
+
+# Guarantee LD_LIBRARY_PATH isn't empty, so we don't have a leading colon in it during a later export (which
+#  would lead to a security hole where Linux first searches the current directory for libraries!)
+echo 'export LD_LIBRARY_PATH=/software/mpiexec/lib/' | sudo tee -a /etc/environment
 
 # Set path right now.
-export PATH=$PATH:/software/openmpi/3.1.2/bin
-export LD_LIBRARY_PATH=/software/openmpi/3.1.2/lib/
+export PATH=$PATH:/software/mpiexec/bin
+export LD_LIBRARY_PATH=/software/mpiexec/lib/
+
+#########
+
+## We run MPI using the network installation, so we only need to add that to our path(s). Make sure all users
+##  have this in their path(s).
+#echo 'export PATH=$PATH:/software/openmpi/3.1.2/bin' | sudo tee -a /etc/skel/.bashrc
+##echo 'export PATH=$PATH:/opt/shared/openmpi/3.1.2/bin' | sudo tee -a /etc/skel/.bashrc
+#echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/openmpi/3.1.2/lib/' | sudo tee -a /etc/skel/.bashrc
+#
+## Add openmpi bin and library to path for everyone.
+#sudo sed -i '/^PATH/ s/\"$/\:\/software\/openmpi\/3.1.2\/bin\"/g' /etc/environment
+#echo 'LD_LIBRARY_PATH=/software/openmpi/3.1.2/lib/' | sudo tee -a /etc/environment
+#
+## Set path right now.
+#export PATH=$PATH:/software/openmpi/3.1.2/bin
+#export LD_LIBRARY_PATH=/software/openmpi/3.1.2/lib/
