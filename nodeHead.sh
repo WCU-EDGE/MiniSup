61f7481b6a07faa9f48ba4b5ee19c94fa76989e2
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-17 04:45:20 -0500

    add installLdapClient

--- nodeHead.sh
+++ nodeHead.sh
@@ -6,6 +6,9 @@ echo 'nodeHead.sh'
 # Install LDAP server
 sudo /local/repository/ldap/installLdapHead.sh
 
+# Install LDAP client so we can run MPI as LDAP users
+sudo /local/repository/ldap/installLdapClient.sh
+
 # Install NFS server
 sudo /local/repository/nfs/installNfsHead.sh $1 $2
 
