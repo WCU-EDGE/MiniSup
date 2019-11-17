b6ba4a5cd316e52e52b535535ee64a89da0c1a44
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-17 08:25:10 -0500

    login node gets routable ip

--- profile.py
+++ profile.py
@@ -45,13 +45,13 @@ link = request.LAN("lan")
 for i in range(0,params.n + 4):
   if i == 0:
     node = request.XenVM("head")
-    node.routable_control_ip = "true"
   elif i == beegfnNum:
     node = request.XenVM("beenode")
   elif i == slurmNum:
     node = request.XenVM("slurmnode")
   elif i == loginNum:
     node = request.XenVM("login")
+    node.routable_control_ip = "true"    
   else:
     node = request.XenVM("worker-" + str(i))
   node.cores = 4
