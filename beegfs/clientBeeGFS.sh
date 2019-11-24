859080bb6fbe91515c116a92c92590d1fd60d456
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-24 08:09:33 -0500

    rename beenode --> pfs on client

--- beegfs/clientBeeGFS.sh
+++ beegfs/clientBeeGFS.sh
@@ -4,18 +4,14 @@
 
 echo 'clientBeeGFS.sh'
 
-#sudo wget -O /etc/apt/sources.list.d/beegfs-deb9.list https://www.beegfs.io/release/latest-stable/dists/beegfs-deb9.list
-
-## The next line is the contents of the beegfs-deb8.list file from beegfs.io, which isn't always available for some reason.
-#sudo sh -c "echo 'deb [arch=amd64] https://www.beegfs.io/release/beegfs_7_1 jessie non-free' >> /etc/apt/sources.list.d/beegfs-deb8.list"
-
 sudo cp /local/repository/beegfs/beegfs-deb8.list /etc/apt/sources.list.d/beegfs-deb8.list
 
 sudo wget -q https://www.beegfs.io/release/latest-stable/gpg/DEB-GPG-KEY-beegfs -O- | sudo apt-key add -
 sudo apt-get update -y
 sudo apt-get install -y beegfs-helperd beegfs-utils
 sudo apt-get install -y beegfs-client
-sudo /opt/beegfs/sbin/beegfs-setup-client -m beenode
+#sudo /opt/beegfs/sbin/beegfs-setup-client -m beenode
+sudo /opt/beegfs/sbin/beegfs-setup-client -m pfs
 
 sudo systemctl start beegfs-helperd
 sudo systemctl start beegfs-client
