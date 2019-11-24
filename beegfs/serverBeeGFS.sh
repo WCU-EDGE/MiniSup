3ce4225664b8db64f0791e8dd2475fc76528f188
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-24 08:08:11 -0500

    rename beenode --> pfs

--- beegfs/serverBeeGFS.sh
+++ beegfs/serverBeeGFS.sh
@@ -14,9 +14,13 @@ sudo apt-get install -y beegfs-meta
 sudo apt-get install -y beegfs-storage
 sudo apt-get install -y beegfs-client beegfs-helperd beegfs-utils
 
+#sudo /opt/beegfs/sbin/beegfs-setup-mgmtd -p /data/beegfs/beegfs_mgmtd
+#sudo /opt/beegfs/sbin/beegfs-setup-meta -p /data/beegfs/beegfs_meta -s 2 -m beenode
+#sudo /opt/beegfs/sbin/beegfs-setup-storage -p /mnt/myraid1/beegfs_storage -s 3 -i 301 -m beenode
+
 sudo /opt/beegfs/sbin/beegfs-setup-mgmtd -p /data/beegfs/beegfs_mgmtd
-sudo /opt/beegfs/sbin/beegfs-setup-meta -p /data/beegfs/beegfs_meta -s 2 -m beenode
-sudo /opt/beegfs/sbin/beegfs-setup-storage -p /mnt/myraid1/beegfs_storage -s 3 -i 301 -m beenode
+sudo /opt/beegfs/sbin/beegfs-setup-meta -p /data/beegfs/beegfs_meta -s 2 -m pfs
+sudo /opt/beegfs/sbin/beegfs-setup-storage -p /mnt/myraid1/beegfs_storage -s 3 -i 301 -m pfs
 
 sudo systemctl start beegfs-mgmtd
 sudo systemctl start beegfs-meta
