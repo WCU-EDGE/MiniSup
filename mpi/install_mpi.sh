f4495a1461dfe6cc37e280a3cd2df371d7312f3c
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-26 03:05:52 -0500

    switch to openmpi

--- mpi/install_mpi.sh
+++ mpi/install_mpi.sh
@@ -6,35 +6,28 @@ USERNAMELIST=$(getent passwd {1000..60000} | sed 's/:.*//')
 
 set -x
 
-wget http://www.mpich.org/static/downloads/3.3.2/mpich-3.3.2.tar.gz
-tar -xzf mpich-3.3.2.tar.gz
-cd mpich-3.3.2
-./configure --disable-fortran --prefix=/software/mpi --exec-prefix=/software/mpiexec --with-slurm=/software/slurm --with-pmi=pmi2
-
-#./configure --disable-fortran --prefix=/software/mpi --exec-prefix=/software/mpiexec
-
-### Will want to use this command instead of the one right above, to use Slurm
-#./configure --disable-fortran --prefix=/software/mpi --exec-prefix=/software/mpiexec --with-slurm=PATH [and four others - use ./configure --help to see them!]
-
-sudo make
-sudo make install
-PATH=$PATH:/software/mpiexec/bin
+sudo wget https://download.open-mpi.org/release/open-mpi/v4.0/openmpi-4.0.2.tar.gz
+gunzip -c openmpi-4.0.2.tar.gz | tar xf -
+cd openmpi-4.0.2
+sudo ./configure --prefix=/software/openmpi --with-slurm --with-pmi=/software/slurm/include/slurm
+sudo make all install
+PATH=$PATH:/software/openmpi/bin
 
 # We run MPI using the network installation, so we need to add that to our path(s). Make sure all users
 #  have this in their path(s).
-echo 'export PATH=$PATH:/software/mpiexec/bin' | sudo tee -a /etc/skel/.bashrc
-echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/mpiexec/lib/' | sudo tee -a /etc/skel/.bashrc
+echo 'export PATH=$PATH:/software/openmpi/bin' | sudo tee -a /etc/skel/.bashrc
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/openmpi/lib/' | sudo tee -a /etc/skel/.bashrc
 
 # Everyone should look to the openmpi binaries.
-sudo sed -i '/^PATH/ s/\"$/\:\/software\/mpiexec\/bin\"/g' /etc/environment
+sudo sed -i '/^PATH/ s/\"$/\:\/software\/openmpi\/bin\"/g' /etc/environment
 
 # Guarantee LD_LIBRARY_PATH isn't empty, so we don't have a leading colon in it during a later export (which
 #  would lead to a security hole where Linux first searches the current directory for libraries!)
-echo 'export LD_LIBRARY_PATH=/software/mpiexec/lib/' | sudo tee -a /etc/environment
+echo 'export LD_LIBRARY_PATH=/software/openmpi/lib/' | sudo tee -a /etc/environment
 
 # Set path right now.
-export PATH=$PATH:/software/mpiexec/bin
-export LD_LIBRARY_PATH=/software/mpiexec/lib/
+export PATH=$PATH:/software/openmpi/bin
+export LD_LIBRARY_PATH=/software/openmpi/lib/
 
 # Make sure everyone picks up the paths!
 for i in $USERNAMELIST 
@@ -42,18 +35,17 @@ do
     sudo mkdir /home/$i || true
     sudo chown $i /home/$i
     sudo touch /home/$i/.bashrc
-    echo 'export PATH=$PATH:/software/mpiexec/bin' | sudo tee -a /home/$i/.bashrc
-    echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/mpiexec/lib/' | sudo tee -a /home/$i/.bashrc
+    echo 'export PATH=$PATH:/software/openmpi/bin' | sudo tee -a /home/$i/.bashrc
+    echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/openmpi/lib/' | sudo tee -a /home/$i/.bashrc
     sudo mkdir /users/$i || true
     sudo chown $i /users/$i
     sudo touch /users/$i/.bashrc
-    echo 'export PATH=$PATH:/software/mpiexec/bin' | sudo tee -a /users/$i/.bashrc
-    echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/mpiexec/lib/' | sudo tee -a /users/$i/.bashrc
+    echo 'export PATH=$PATH:/software/openmpi/bin' | sudo tee -a /users/$i/.bashrc
+    echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/openmpi/lib/' | sudo tee -a /users/$i/.bashrc
 done
 
 # Create the machine list for MPI workers to use
 MACHINELISTFILE=/mpishare/machinelist
-#echo "login" | sudo tee -a $MACHINELISTFILE
 for m in $(seq 1 $1)
 do
   echo "worker-${m}" | sudo tee -a $MACHINELISTFILE
@@ -61,8 +53,70 @@ done
 sudo chmod 755 $MACHINELISTFILE 
 
 cd ..
-sudo rm -Rf mpich-3.3.2
-sudo rm -Rf mpich-3.3.2.tar.gz
+sudo rm -Rf openmpi-4.0.2
+sudo rm -Rf openmpi-4.0.2.tar.gz
+
+######################
+
+#wget http://www.mpich.org/static/downloads/3.3.2/mpich-3.3.2.tar.gz
+#tar -xzf mpich-3.3.2.tar.gz
+#cd mpich-3.3.2
+#
+#./configure --disable-fortran --prefix=/software/mpi --exec-prefix=/software/mpiexec --with-slurm=/software/slurm --with-slurm-include=/software/slurm/include --with-slurm-lib=/software/slurm/lib --with-pmi=pmi2 --with-pm=slurm
+#
+##./configure --disable-fortran --prefix=/software/mpi --exec-prefix=/software/mpiexec --with-slurm=/software/slurm --with-pmi=pmi2
+##./configure --disable-fortran --prefix=/software/mpi --exec-prefix=/software/mpiexec
+#
+#### Will want to use this command instead of the one right above, to use Slurm
+##./configure --disable-fortran --prefix=/software/mpi --exec-prefix=/software/mpiexec --with-slurm=PATH [and four others - use ./configure --help to see them!]
+#
+#sudo make
+#sudo make install
+#PATH=$PATH:/software/mpiexec/bin
+#
+## We run MPI using the network installation, so we need to add that to our path(s). Make sure all users
+##  have this in their path(s).
+#echo 'export PATH=$PATH:/software/mpiexec/bin' | sudo tee -a /etc/skel/.bashrc
+#echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/mpiexec/lib/' | sudo tee -a /etc/skel/.bashrc
+#
+## Everyone should look to the openmpi binaries.
+#sudo sed -i '/^PATH/ s/\"$/\:\/software\/mpiexec\/bin\"/g' /etc/environment
+#
+## Guarantee LD_LIBRARY_PATH isn't empty, so we don't have a leading colon in it during a later export (which
+##  would lead to a security hole where Linux first searches the current directory for libraries!)
+#echo 'export LD_LIBRARY_PATH=/software/mpiexec/lib/' | sudo tee -a /etc/environment
+#
+## Set path right now.
+#export PATH=$PATH:/software/mpiexec/bin
+#export LD_LIBRARY_PATH=/software/mpiexec/lib/
+#
+## Make sure everyone picks up the paths!
+#for i in $USERNAMELIST 
+#do
+#    sudo mkdir /home/$i || true
+#    sudo chown $i /home/$i
+#    sudo touch /home/$i/.bashrc
+#    echo 'export PATH=$PATH:/software/mpiexec/bin' | sudo tee -a /home/$i/.bashrc
+#    echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/mpiexec/lib/' | sudo tee -a /home/$i/.bashrc
+#    sudo mkdir /users/$i || true
+#    sudo chown $i /users/$i
+#    sudo touch /users/$i/.bashrc
+#    echo 'export PATH=$PATH:/software/mpiexec/bin' | sudo tee -a /users/$i/.bashrc
+#    echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/mpiexec/lib/' | sudo tee -a /users/$i/.bashrc
+#done
+#
+## Create the machine list for MPI workers to use
+#MACHINELISTFILE=/mpishare/machinelist
+##echo "login" | sudo tee -a $MACHINELISTFILE
+#for m in $(seq 1 $1)
+#do
+#  echo "worker-${m}" | sudo tee -a $MACHINELISTFILE
+#done
+#sudo chmod 755 $MACHINELISTFILE 
+#
+#cd ..
+#sudo rm -Rf mpich-3.3.2
+#sudo rm -Rf mpich-3.3.2.tar.gz
 
 ########
 
