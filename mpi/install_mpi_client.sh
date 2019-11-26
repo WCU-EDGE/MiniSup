dfcfa0c03be7bca681767d7681f90932aa0b3cfc
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-26 03:06:56 -0500

    to openmpi

--- mpi/install_mpi_client.sh
+++ mpi/install_mpi_client.sh
@@ -6,23 +6,23 @@ USERNAMELIST=$(getent passwd {1000..60000} | sed 's/:.*//')
 
 set -x
 
-PATH=$PATH:/software/mpiexec/bin
+PATH=$PATH:/software/openmpi/bin
 
 # We run MPI using the network installation, so we need to add that to our path(s). Make sure all users
 #  have this in their path(s).
-echo 'export PATH=$PATH:/software/mpiexec/bin' | sudo tee -a /etc/skel/.bashrc
-echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/mpiexec/lib/' | sudo tee -a /etc/skel/.bashrc
+echo 'export PATH=$PATH:/software/openmpi/bin' | sudo tee -a /etc/skel/.bashrc
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/openmpi/lib/' | sudo tee -a /etc/skel/.bashrc
 
 # Everyone should look to the openmpi binaries.
-sudo sed -i '/^PATH/ s/\"$/\:\/software\/mpiexec\/bin\"/g' /etc/environment
+sudo sed -i '/^PATH/ s/\"$/\:\/software\/openmpi\/bin\"/g' /etc/environment
 
 # Guarantee LD_LIBRARY_PATH isn't empty, so we don't have a leading colon in it during a later export (which
 #  would lead to a security hole where Linux first searches the current directory for libraries!)
-echo 'export LD_LIBRARY_PATH=/software/mpiexec/lib/' | sudo tee -a /etc/environment
+echo 'export LD_LIBRARY_PATH=/software/openmpi/lib/' | sudo tee -a /etc/environment
 
 # Set path right now.
-export PATH=$PATH:/software/mpiexec/bin
-export LD_LIBRARY_PATH=/software/mpiexec/lib/
+export PATH=$PATH:/software/openmpi/bin
+export LD_LIBRARY_PATH=/software/openmpi/lib/
 
 # Make sure everyone picks up the paths!
 for i in $USERNAMELIST 
@@ -30,13 +30,13 @@ do
     sudo mkdir /home/$i || true
     sudo chown $i /home/$i
     sudo touch /home/$i/.bashrc
-    echo 'export PATH=$PATH:/software/mpiexec/bin' | sudo tee -a /home/$i/.bashrc
-    echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/mpiexec/lib/' | sudo tee -a /home/$i/.bashrc
+    echo 'export PATH=$PATH:/software/openmpi/bin' | sudo tee -a /home/$i/.bashrc
+    echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/openmpi/lib/' | sudo tee -a /home/$i/.bashrc
     sudo mkdir /users/$i || true
     sudo chown $i /users/$i
     sudo touch /users/$i/.bashrc
-    echo 'export PATH=$PATH:/software/mpiexec/bin' | sudo tee -a /users/$i/.bashrc
-    echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/mpiexec/lib/' | sudo tee -a /users/$i/.bashrc
+    echo 'export PATH=$PATH:/software/openmpi/bin' | sudo tee -a /users/$i/.bashrc
+    echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/software/openmpi/lib/' | sudo tee -a /users/$i/.bashrc
 done
 
 #########
