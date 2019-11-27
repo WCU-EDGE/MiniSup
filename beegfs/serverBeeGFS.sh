2a9f999f2ddd14be8e6887e109654ab3598e5059
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-27 10:19:51 -0500

    remove commented code

--- beegfs/serverBeeGFS.sh
+++ beegfs/serverBeeGFS.sh
@@ -4,8 +4,6 @@
 
 echo 'serverBeeGFS.sh'
 
-### Name the machine beenode
-
 sudo wget -O /etc/apt/sources.list.d/beegfs-deb9.list https://www.beegfs.io/release/latest-stable/dists/beegfs-deb9.list
 sudo wget -q https://www.beegfs.io/release/latest-stable/gpg/DEB-GPG-KEY-beegfs -O- | sudo apt-key add -
 sudo apt-get update -y
@@ -13,10 +11,6 @@ sudo apt-get install -y beegfs-mgmtd
 sudo apt-get install -y beegfs-meta
 sudo apt-get install -y beegfs-storage
 sudo apt-get install -y beegfs-client beegfs-helperd beegfs-utils
-
-#sudo /opt/beegfs/sbin/beegfs-setup-mgmtd -p /data/beegfs/beegfs_mgmtd
-#sudo /opt/beegfs/sbin/beegfs-setup-meta -p /data/beegfs/beegfs_meta -s 2 -m beenode
-#sudo /opt/beegfs/sbin/beegfs-setup-storage -p /mnt/myraid1/beegfs_storage -s 3 -i 301 -m beenode
 
 sudo /opt/beegfs/sbin/beegfs-setup-mgmtd -p /data/beegfs/beegfs_mgmtd
 sudo /opt/beegfs/sbin/beegfs-setup-meta -p /data/beegfs/beegfs_meta -s 2 -m pfs
