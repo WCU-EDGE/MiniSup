bb29f457097cbaa76e971d404a3c72833896292c
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-27 10:27:02 -0500

    slurm node = head

--- nfs/installNfsHead.sh
+++ nfs/installNfsHead.sh
@@ -41,7 +41,7 @@ do
   echo "/software 192.168.1.$i(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 done
 
-# Let the Slurm node have access to the shared directories.
+# Let the Slurm node (head) have access to the shared directories.
 slurmip=$(($2 + 1))
 echo "/home 192.168.1.$slurmip(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
 echo "/mpishare 192.168.1.$slurmip(rw,sync,no_root_squash,no_subtree_check)" | sudo tee -a /etc/exports
