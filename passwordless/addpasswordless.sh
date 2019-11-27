a29a25c821ca3e663abe8e16daf17c0a96c5f31d
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-27 10:28:15 -0500

    remove commented code

--- passwordless/addpasswordless.sh
+++ passwordless/addpasswordless.sh
@@ -27,23 +27,6 @@ fi
 # The passwordless script must run for every user so put it in .bashrc.
 for u in $USERNAMELIST 
 do
-    #if [ ! -f /users/$i/.ssh/id_rsa ]
-    #then
-    #   USER_GROUP=`id -gn ${i}`
-    #
-    #   sudo mkdir /home/$i || true
-    #   sudo chown $i:$USER_GROUP /home/$i
-    #   sudo touch /home/$i/.bashrc
-    #   sudo chown $i:$USER_GROUP /home/$i/.bashrc
-    #   echo "bash /local/repository/passwordless/passwordless.sh $1" | sudo tee -a /home/$i/.bashrc
-    #   
-    #   sudo mkdir /users/$i || true
-    #   sudo chown $i:$USER_GROUP /users/$i
-    #   sudo touch /users/$i/.bashrc
-    #   sudo chown $i:$USER_GROUP /users/$i/.bashrc
-    #   echo "bash /local/repository/passwordless/passwordless.sh $1" | sudo tee -a /users/$i/.bashrc
-    #fi
-    
     sudo usermod -a -G root $u
     sudo -H -u $u bash -c "/local/repository/passwordless/passwordless.sh $1"
 done
