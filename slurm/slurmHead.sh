8d5308aade24e79498bd4bd9f7de3d812edaf0e9
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-11-27 10:35:51 -0500

    remove test code, comment out useradd

--- slurm/slurmHead.sh
+++ slurm/slurmHead.sh
@@ -6,19 +6,13 @@ echo 'slurmHead.sh'
 sudo apt-get update -y
 
 #HOSTNAMETRIM=$(echo $HOSTNAME | awk -F'.' '{print $1}')
-#sudo sed -i "s/ControlMachine=slurm-ctrl/ControlMachine=$HOSTNAMETRIM/g" ubuntu-slurm/slurm.conf
+#sudo sed -i "s/ControlMachine=slurm-ctrl/ControlMachine=$HOSTNAMETRIM/g" /local/repository/slurm/slurm.conf
 SLURMMACHINELIST=worker-[1-$(($1))]
 sudo sed -i "s/NodeName=linux1/NodeName=$SLURMMACHINELIST/g" /local/repository/slurm/slurm.conf
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y git gcc make ruby ruby-dev libpam0g-dev libmariadb-client-lgpl-dev libmysqlclient-dev
 sudo gem install fpm
 cd /software
-
-#sudo git clone https://github.com/mknoxnv/ubuntu-slurm.git
-#HOSTNAMETRIM=$(echo $HOSTNAME | awk -F'.' '{print $1}')
-#sudo sed -i "s/ControlMachine=slurm-ctrl/ControlMachine=$HOSTNAMETRIM/g" ubuntu-slurm/slurm.conf
-#SLURMMACHINELIST=worker-[1-$(($1))]
-#sudo sed -i "s/NodeName=linux1/NodeName=$SLURMMACHINELIST/g" ubuntu-slurm/slurm.conf
 
 sudo apt-get install -y libmunge-dev libmunge2 munge
 sudo systemctl enable munge
@@ -41,13 +35,10 @@ EOF
 
 sudo mysql -u root < /tmp/setupmaria.sql
 
-#cd /opt
 cd /software
 sudo wget https://download.schedmd.com/slurm/slurm-17.11.12.tar.bz2
 sudo tar xvjf slurm-17.11.12.tar.bz2
 cd slurm-17.11.12
-#sudo ./configure --prefix=/tmp/slurm-build --sysconfdir=/etc/slurm --enable-pam --with-pam_dir=/lib/x86_64-linux-gnu/security/ --without-shared-libslurm
-#sudo ./configure --prefix=/software/slurm --sysconfdir=/etc/slurm --enable-pam --with-pam_dir=/lib/x86_64-linux-gnu/security/ --without-shared-libslurm
 sudo ./configure --prefix=/software/slurm --sysconfdir=/etc/slurm --enable-pam --with-pam_dir=/lib/x86_64-linux-gnu/security/ --without-shared-libslurm --with-pmix
 sudo make
 sudo make contrib
@@ -56,7 +47,7 @@ cd ..
 sudo fpm -s dir -t deb -v 1.0 -n slurm-17.11.12 --prefix=/usr -C /software/slurm .
 
 sudo dpkg -i slurm-17.11.12_1.0_amd64.deb  
-sudo useradd slurm
+#sudo useradd slurm
 sudo mkdir -p /etc/slurm /etc/slurm/prolog.d /etc/slurm/epilog.d /var/spool/slurm/ctld /var/spool/slurm/d /var/log/slurm
 sudo chown slurm /var/spool/slurm/ctld /var/spool/slurm/d /var/log/slurm
 
