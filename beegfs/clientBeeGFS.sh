4e825a149715c22b47ec2f5ec229820a4e0979bf
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2019-12-09 04:45:25 -0500

    get mgmt server from JSON

--- beegfs/clientBeeGFS.sh
+++ beegfs/clientBeeGFS.sh
@@ -4,15 +4,21 @@
 
 echo 'clientBeeGFS.sh'
 
-BEEGFS_SERVER=$1
-
 sudo cp /local/repository/beegfs/beegfs-deb8.list /etc/apt/sources.list.d/beegfs-deb8.list
 
 sudo wget -q https://www.beegfs.io/release/latest-stable/gpg/DEB-GPG-KEY-beegfs -O- | sudo apt-key add -
 sudo apt-get update -y
+sudo apt-get install -y jq
 sudo apt-get install -y beegfs-helperd beegfs-utils
 sudo apt-get install -y beegfs-client
-sudo /opt/beegfs/sbin/beegfs-setup-client -m $BEEGFS_SERVER
+
+# Get the mgmt server from the JSON file.  sed trick to remove quotation marks from https://stackoverflow.com/questions/9733338/shell-script-remove-first-and-last-quote-from-a-variable
+PFS_JSON_FILE=/local/repository/beegfs/pfs_servers.json
+BEEGFS_MGMT_SVR_RAW=$(jq ".[] | select(.type == \"mgmt\") | .nodename" $PFS_JSON_FILE)
+BEEGFS_MGMT_SERVER=$(sed -e 's/^"//' -e 's/"$//' <<<"$BEEGFS_MGMT_SVR_RAW")
+
+# Point to the mgmt server.
+sudo /opt/beegfs/sbin/beegfs-setup-client -m $BEEGFS_MGMT_SERVER
 
 sudo systemctl start beegfs-helperd
 sudo systemctl start beegfs-client
