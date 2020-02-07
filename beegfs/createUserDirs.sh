3785e9244352679d67c711428714aeb41ab7caa5
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2020-02-06 20:01:34 -0500

    Create createUserDirs.sh

---
+++ beegfs/createUserDirs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "createUserDirs.sh"
+
+USERNAMELIST=$(getent passwd {1000..60000} | sed 's/:.*//')
+
+set -x
+
+# Create a directory for every user.
+for u in $USERNAMELIST 
+do
+    if [ ! -d /scratch/$u ]; then
+       # Whoever gets to this code first does the work.
+       sudo mkdir /scratch/$u
+        
+       # Copy the source files from the repository, if they exist.
+       sudo cp /local/repository/source/* /scratch/$u || true
+       sudo chmod 755 /scratch/$u/*
+    
+       # Give ownership to the user
+       sudo chown -hR $u /scratch/$u
+    fi
+done
