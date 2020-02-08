c76b9ee0c08885c13e90861127ca733296de6f6a
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2020-02-07 21:27:30 -0500

    workers: move beegfs client later in install

--- profile.py
+++ profile.py
@@ -99,8 +99,8 @@ for i in range(0,machineCount):
     node.addService(pg.Execute(shell="sh", command="sudo /local/repository/passwordless/addpasswordless.sh " + str(params.n)))
   else:
     node.addService(pg.Execute(shell="sh", command="sudo /local/repository/workers/nodeWorker.sh"))
-    node.addService(pg.Execute(shell="sh", command="sudo /local/repository/beegfs/clientBeeGFS.sh"))
     node.addService(pg.Execute(shell="sh", command="sudo /local/repository/slurm/slurmClient.sh " + str(params.n)))
+    node.addService(pg.Execute(shell="sh", command="sudo /local/repository/beegfs/clientBeeGFS.sh"))
     node.addService(pg.Execute(shell="sh", command="sudo /local/repository/passwordless/addpasswordless.sh " + str(params.n)))
   
 # Print the RSpec to the enclosing page.
