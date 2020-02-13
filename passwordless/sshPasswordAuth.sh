f6a22dc738442e9989412c3d19c7b85e6352b851
Jon Kilgannon <jkilgannon@users.noreply.github.com>
2020-02-13 13:36:24 -0500

    Create sshPasswordAuth.sh

---
+++ passwordless/sshPasswordAuth.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "sshPasswordAuth.sh"
+
+# Script will set SSH to allow password authentication, which allows non-Cloudlab
+# users (LDAP users) to log into the supercomputer.  This will be run on the head node.
+
+set -x
+
+sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+
+sleep 5
+sudo systemctl restart sshd
+sleep 30
+STARTOUTPUT=$(sudo systemctl status sshd)
+STARTWORKED=$(echo $STARTOUTPUT | grep "Active: active (running)")
+while [ -z "$STARTWORKED" ]; do
+   sudo systemctl restart sshd
+   sleep 30
+   STARTOUTPUT=$(sudo systemctl status sshd)
+   STARTWORKED=$(echo $STARTOUTPUT | grep "Active: active (running)")
+done
+
+echo "SSH server restarted."
